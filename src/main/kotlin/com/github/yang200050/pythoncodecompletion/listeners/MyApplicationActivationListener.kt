b0fcf48f3de56d41d459b4de767084dfc835e323
GitHub Action <action@github.com>
2023-12-10 12:14:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/yang200050/pythoncodecompletion/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/yang200050/pythoncodecompletion/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yang200050.pythoncodecompletion.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
